a2c6f73783777e3a54423a3d235e61777be99482
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2025-10-27 16:23:20 +0300

    Add helpers for transfer APIs

---
+++ Include/nosTransfer/Transfer.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "nosTransfer.h"
+
+#include <Nodos/Plugin.hpp>
+
+namespace  nos::transfer
+{
+struct Slot
+{
+	nosTransferCopyDestination Handle;
+	Slot(ObjectRef src)
+	{
+		auto res = nosTransfer->CreateCopyDestination(src, &Handle);
+		NOS_SOFT_CHECK(res == NOS_RESULT_SUCCESS, "Failed to create transfer copy destination slot");
+	}
+	~Slot()
+	{
+		nosTransfer->ReleaseCopyDestination(Handle);
+	}
+	Slot(const Slot&) = delete;
+	Slot& operator=(const Slot&) = delete;
+	nosResult CopyFrom(nosObjectId obj)
+	{
+		return nosTransfer->Copy(obj, Handle);
+	}
+	bool IsDestinationCompatibleWith(nosObjectId obj) const
+	{
+		return nosTransfer->CanCopy(obj, Handle) == NOS_TRUE;
+	}
+	ObjectRef GetObject() const
+	{
+		ObjectRef obj{};
+		if (nosTransfer->GetObjectReference(Handle, &obj.GetStorage()) != NOS_RESULT_SUCCESS)
+			return ObjectRef();
+		return obj;
+	}
+};
+}
